8abb4a33e1810a7af0c78b21eda6a3fb2c2fa0e6
kudzaimhlanga <37145094+kudzaimhlanga@users.noreply.github.com>
2024-12-01 19:52:51 +0200

    Update counter up

--- Global/counter.js
+++ Global/counter.js
@@ -1,12 +1,45 @@
 window.addEventListener('load', () => {
-	gsap.registerPlugin(ScrollTrigger);
+	gsap.registerPlugin(ScrollTrigger) 
 
 	const counterElements = [...document.querySelectorAll('.counterup')];
 
 	if (counterElements.length > 0) {
 		for (let i = 0; i < counterElements.length; i++) {
 			addTrigger(counterElements[i]);
+			// scramble(counterElements[i])
 		}
+	}
+
+
+	// function scramble(element){
+	// 	const nums = getRandNumbers(element);
+		
+	// 	const tl = new gsap.timeline({
+	// 		defaults: {
+	// 			duration: 2,
+	// 			ease: "linear"
+	// 		}
+	// 	});
+
+	// 	console.log(nums)
+
+	// 	tl.from(element, {scrambleText: {
+	// 		chars: "0123457890",
+	// 		revealDelay: 1,
+	// 		speed: 2
+	// 	}})
+	// }
+
+	function getRandNumbers(element){
+		const elementText = String(element.innerText);
+		let num = '';
+		
+		for(let i = 0; i < elementText.length; i++){
+			const randNum = Math.floor(Math.random() * 9);
+			num += `${randNum}`
+		}
+
+		return num;
 	}
 
 	function addTrigger(element) {
@@ -20,4 +53,5 @@ window.addEventListener('load', () => {
 			},
 		});
 	}
+
 });
