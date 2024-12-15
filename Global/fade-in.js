d1dfdd525850954cbd160ed10a4be0d016b60909
kudzaimhlanga <37145094+kudzaimhlanga@users.noreply.github.com>
2024-12-15 14:04:38 +0200

    added video assets

---
+++ Global/fade-in.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+	const fadeInElements = [...document.querySelectorAll('.fade-in')];
+
+	if (fadeInElements.length > 0) {
+		animateElements(fadeInElements);
+	}
+
+	function animateElements(elements) {
+		gsap.set(elements, { opacity: 1, autoAlpha: 1 });
+		gsap.from(elements, {
+			y: '2.5rem',
+			opacity: 0,
+			duration: 0.5,
+			ease: 'power2.out',
+            stagger: {
+                each: 0.05
+            }
+		});
+	}
+});
